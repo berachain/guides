f35ebdde130fe185afb40b8ca9ada887216165f3
dethebera <utkarshsharma1189@gmail.com>
2024-02-09 18:16:33 +0530

    adding new guide on Hardhat Ethers - Contract Verification

---
+++ apps/hardhat-contract-verification/scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { setBlockGasLimit } from '@nomicfoundation/hardhat-toolbox/network-helpers';
+import { ethers } from 'hardhat';
+
+async function main() {
+  const nft = await ethers.deployContract('NFT');
+  
+
+  await nft.waitForDeployment();
+
+  console.log('NFT Contract Deployed at ' + nft.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
