3cd89486b50eb834dbb91b165f22c3578c1c3ce9
Nealsen <soumitrarocking05@gmail.com>
2024-02-15 13:58:44 +0530

    Update HelloWorld.sol
    Access Control: Added a onlyOwner modifier to restrict the ability to change the greeting to the owner of the contract (the one who deployed it).
    
    Owner Variable: Introduced an owner state variable to keep track of the contract owner.
    
    Transfer Ownership Function: An optional function to change the owner of the contract. This is useful if you ever need to transfer control of the contract to a different address.
    
    Error Handling: Added require statements for validating conditions (like non-zero new owner address).
    
    Event Indexing: Added indexed to the sender in the event for easier filtering and searching.
    
    Signed-off-by: Nealsen <soumitrarocking05@gmail.com>

--- apps/ethers6-solc-helloworld/contracts/HelloWorld.sol
+++ apps/ethers6-solc-helloworld/contracts/HelloWorld.sol
@@ -2,26 +2,42 @@
 pragma solidity ^0.8.9;
 
 contract HelloWorld {
-    // Events that allows for emitting a message
-    event NewGreeting(address sender, string message);
+    // Event that allows for emitting a message when the greeting is changed
+    event NewGreeting(address indexed sender, string message);
 
-    // Variables
-    string greeting;
+    // Variable to store the greeting message
+    string private greeting;
+
+    // Address of the contract owner
+    address private owner;
 
     // Main constructor run at deployment
     constructor(string memory _greeting) {
         greeting = _greeting;
+        owner = msg.sender; // Setting the contract deployer as the owner
         emit NewGreeting(msg.sender, _greeting);
     }
 
-    // Get function
+    // Modifier to restrict access to the owner only
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can change the greeting");
+        _;
+    }
+
+    // Function to get the current greeting
     function getGreeting() public view returns (string memory) {
         return greeting;
     }
 
-    // Set function
-    function setGreeting(string memory _greeting) public {
+    // Function to set a new greeting; restricted to owner
+    function setGreeting(string memory _greeting) public onlyOwner {
         greeting = _greeting;
         emit NewGreeting(msg.sender, _greeting);
+    }
+
+    // Optional: Function to transfer ownership of the contract
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner cannot be the zero address");
+        owner = newOwner;
     }
 }
