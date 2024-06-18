f2e9dd7e9eaf49ff1c4dc8f10634f83a1b3d4caa
Larry the Cucumber <larrythecucumber321@gmail.com>
2024-06-18 13:46:46 -0700

    update Berps trading bot for bArtio

--- apps/berps-bot/src/config.ts
+++ apps/berps-bot/src/config.ts
@@ -11,7 +11,7 @@ export const CONFIG = {
   DATA_INTERVAL: parseFloat(process.env.DATA_INTERVAL || "5"),
   BOLLINGER_PERIOD: parseFloat(process.env.BOLLINGER_PERIOD || "20"),
   BOLLINGER_MULTIPLIER: parseFloat(process.env.BOLLINGER_MULTIPLIER || "2"),
-  TRADING_CONTRACT_ADDRESS: process.env.TRADING_CONTRACT_ADDRESS || "",
+  ENTRYPOINT_CONTRACT_ADDRESS: process.env.ENTRYPOINT_CONTRACT_ADDRESS || "",
   PRIVATE_KEY: process.env.PRIVATE_KEY || "",
   RPC_PROVIDER: process.env.RPC_PROVIDER || "",
 };
