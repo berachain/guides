c23b97dfb0fedaad87648d71ef6c661b31514129
leirbag95 <gabrielelfassi@yahoo.fr>
2024-08-09 15:52:18 +0200

    [+] gelato-vrf/ repo added

---
+++ apps/gelato-vrf/deploy/SimpleVRFContract.deploy.ts
@@ -0,0 +1,27 @@
+import hre, { deployments, getNamedAccounts } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const isHardhat = hre.network.name === "hardhat";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const dedicatedMsgSender = "0xa55e7F0dD850C5353025d3cFA5a36e648635a256";
+
+  if (!isHardhat) {
+    console.log(
+      `\nDeploying SimpleVRFContract to ${hre.network.name}. Hit ctrl + c to abort`
+    );
+  }
+
+  await deploy("SimpleVRFContract", {
+    from: deployer,
+    log: !isHardhat,
+    args: [dedicatedMsgSender],
+  });
+};
+
+func.tags = ["SimpleVRFContract"];
+
+export default func;
