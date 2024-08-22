1245561c34ff489e83e0ab41a5d67886ffa21462
Larry the Cucumber <larrythecucumber321@gmail.com>
2024-08-22 11:52:08 -0700

    update contracts and tests

--- apps/openzeppelin-upgrades/script/DeployUpgrade.s.sol
+++ apps/openzeppelin-upgrades/script/DeployUpgrade.s.sol
@@ -1,23 +1,24 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.25;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
 
-// import "../src/DeFiTokenV2.sol";
-// import "forge-std/Script.sol";
-// import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import "../src/DeFiTokenV2.sol";
+import "forge-std/Script.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
-// contract DeployAndUpgrade is Script {
-//     function run() public {
-//         address proxy = DEPLOYED_PROXY_ADDRESS;
-//         vm.startBroadcast();
+contract DeployAndUpgrade is Script {
+    function run() public {
+        // Replace with your proxy address
+        address proxy = 0xC9e6086507322ee8ae1D5c283101674588B342f5;
+        vm.startBroadcast();
 
-//         Upgrades.upgradeProxy(
-//             proxy,
-//             "DeFiTokenV2.sol:DeFiTokenV2",
-//             abi.encodeWithSelector(DeFiTokenV2.updateName.selector)
-//         );
+        Upgrades.upgradeProxy(
+            proxy,
+            "DeFiTokenV2.sol:DeFiTokenV2",
+            abi.encodeCall(DeFiTokenV2.initialize, ())
+        );
 
-//         vm.stopBroadcast();
+        vm.stopBroadcast();
 
-//         console.log("Token Name:", DeFiTokenV2(proxy).name());
-//     }
-// }
+        console.log("Token Name:", DeFiTokenV2(proxy).name());
+    }
+}
