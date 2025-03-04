f46fa0c8bda147920b3e0d68a7a62e01000bfee8
Stephen van Egmond <camembear@berachain.com>
2025-03-04 12:11:15 -0500

    rearrange env vars for clarity

--- apps/berachain-node-scripts/env.sh
+++ apps/berachain-node-scripts/env.sh
@@ -1,21 +1,26 @@
 #!/bin/bash 
 
+########
 # CHANGE THESE VALUES
-export CHAIN_SPEC="mainnet"   # or "testnet"
+export CHAIN_SPEC=testnet   # or "testnet"
 export MONIKER_NAME=camembera
 export WALLET_ADDRESS_FEE_RECIPIENT=0x9BcaA41DC32627776b1A4D714Eef627E640b3EF5
 export EL_ARCHIVE_NODE=false # set to true if you want to run an archive node on CL and EL
 export MY_IP=`curl -s canhazip.com`
 
+########
 # VALUES YOU MIGHT WANT TO CHANGE
 export LOG_DIR=$(pwd)/logs
-export JWT_PATH=$BEACOND_CONFIG/jwt.hex
 export BEACOND_BIN=$(command -v beacond || echo $(pwd)/beacond)
+export BEACOND_DATA=$(pwd)/var/beacond
+export BEACOND_CONFIG=$BEACOND_DATA/config  # can't change this. sorry.
+export JWT_PATH=$BEACOND_CONFIG/jwt.hex
+
+# need at least one of these
 export RETH_BIN=$(command -v reth || echo $(pwd)/reth)
 export GETH_BIN=$(command -v geth || echo $(pwd)/geth)
 export NETHERMIND_BIN=$(command -v Nethermind.Runner || echo $(pwd)/Nethermind.Runner)
 export ERIGON_BIN=$(command -v erigon || echo $(pwd)/erigon)
-export BEACOND_DATA=$(pwd)/var/beacond
 
 # Leave this blank to use the default ports for the various services.
 # Set this to a port number (for example, 30000) to 
@@ -29,8 +34,8 @@ export EL_AUTHRPC_PORT=${PORT_BASE:+$(($PORT_BASE+4))}${PORT_BASE:-8551}
 export EL_ETH_PORT=${PORT_BASE:+$(($PORT_BASE+5))}${PORT_BASE:-30303}
 export PROMETHEUS_PORT=${PORT_BASE:+$(($PORT_BASE+6))}${PORT_BASE:-9101}
 
-# LEAVE BELOW ALONE
-export BEACOND_CONFIG=$BEACOND_DATA/config  # can't change this. sorry.
+######
+# LEAVE BELOW ALONE. CAN CHANGE (most) DATA DIRECTORIES
 
 if [[ "$CHAIN_SPEC" == "testnet" ]]; then
     export CHAIN=testnet-beacon-80069
@@ -54,29 +59,28 @@ else
     export SED_OPT='-i'
 fi
 
-# identify the execution client and choose paths; you can override the data directories
-if command -v $RETH_BIN >/dev/null 2>&1; then
+if command -v $RETH_BIN; then
     export RETH_DATA=$(pwd)/var/reth
     export RETH_GENESIS_PATH=$RETH_DATA/genesis.json
 fi  
 
-if command -v $GETH_BIN >/dev/null 2>&1; then
+if command -v $GETH_BIN; then
     export GETH_DATA=$(pwd)/var/geth
     export GETH_GENESIS_PATH=$GETH_DATA/genesis.json
 fi  
 
-if command -v $NETHERMIND_BIN >/dev/null 2>&1; then
+if command -v $NETHERMIND_BIN; then
     export NETHERMIND_CONFIG_DIR=$(pwd)/var/nethermind/config/
     export NETHERMIND_DATA_DIR=$(pwd)/var/nethermind/data/
     export NETHERMIND_GENESIS_PATH="${NETHERMIND_CONFIG_DIR}/eth-nether-genesis.json"
 fi  
 
-if command -v $ERIGON_BIN >/dev/null 2>&1; then
+if command -v $ERIGON_BIN; then
     export ERIGON_DATA=$(pwd)/var/erigon
     export ERIGON_GENESIS_PATH=$ERIGON_DATA/genesis.json
 fi  
 
-if ! command -v $RETH_BIN >/dev/null 2>&1 && ! command -v $GETH_BIN >/dev/null 2>&1 && ! command -v $NETHERMIND_BIN >/dev/null 2>&1 && ! command -v $ERIGON_BIN >/dev/null 2>&1; then
+if ! command -v $RETH_BIN && ! command -v $GETH_BIN && ! command -v $NETHERMIND_BIN && ! command -v $ERIGON_BIN ; then
     echo "Error: No execution client found in PATH"
     echo "Please install either reth, geth, or Nethermind and ensure it is available in your PATH"
     exit 1
