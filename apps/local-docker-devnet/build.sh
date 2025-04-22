f09d4d1bdf8e1b5dc22318ccce16a51724cad1ca
Camembear <camembear@berachain.com>
2025-04-22 15:55:54 -0400

    switch to two dockerfiles, one for custom build, one for official build

--- apps/local-docker-devnet/build.sh
+++ apps/local-docker-devnet/build.sh
@@ -3,7 +3,13 @@
 set -e;
 source env.sh;
 
-docker build -t $DOCKER_IMAGE_BEACOND -f ./Dockerfiles/beacond/Dockerfile --build-arg CHAIN_SPEC=$CHAIN_SPEC .;
+# do custom build if CUSTOM_BIN_BEACOND is set and not empty
+if [ -f "$CUSTOM_BIN_BEACOND" ]; then
+    docker build -t $DOCKER_IMAGE_BEACOND -f ./Dockerfiles/beacond/Dockerfile-custombuild --build-arg CHAIN_SPEC=$CHAIN_SPEC --build-arg CUSTOM_BIN_BEACOND=$CUSTOM_BIN_BEACOND .;
+else
+    docker build -t $DOCKER_IMAGE_BEACOND -f ./Dockerfiles/beacond/Dockerfile-officialbuild --build-arg CHAIN_SPEC=$CHAIN_SPEC .;
+fi
+
 docker build -t $DOCKER_IMAGE_RETH -f ./Dockerfiles/reth/Dockerfile .;
 docker build -t $DOCKER_IMAGE_CURL -f ./Dockerfiles/curl/Dockerfile .;
 
