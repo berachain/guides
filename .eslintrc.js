4203bf1f6bcac8a7fb372df539d15a6838f07108
jintao <hellojintao@protonmail.com>
2025-05-18 21:05:46 -0500

    chore: remove scripts folder and commit pending changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,4 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@berachain/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
 };
