23e8511ba6ea0919c40bc0b2c72d1164bb89ee7a
jintao <hellojintao@protonmail.com>
2025-05-18 20:12:38 -0500

    chore: update build process and add random project ID for WalletConnect

--- apps/rainbowkit-vite/src/polyfills.ts
+++ apps/rainbowkit-vite/src/polyfills.ts
@@ -1,9 +1,11 @@
 // Polyfills for browser environment
 window.global = window.global ?? window;
-window.Buffer = window.Buffer ?? (() => {
-  const buffer = require('buffer');
-  return buffer.Buffer;
-})();
+window.Buffer =
+  window.Buffer ??
+  (() => {
+    const buffer = require("buffer");
+    return buffer.Buffer;
+  })();
 window.process = window.process ?? { env: {} }; // Minimal process polyfill
 
 export {};
