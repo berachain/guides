6c85ac8ded6ef469b330f7a6ffa7e77816c034d9
jintao <hellojintao@protonmail.com>
2025-05-18 19:40:19 -0500

    chore: update workspace configuration and package naming

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["simple-import-sort"],
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/",
+    "build/",
+    ".turbo/",
+    "coverage/",
+    "*.log",
+  ],
+}; 
