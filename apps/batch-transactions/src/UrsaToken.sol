302f58119ca6975da55448effff3dd210ac2a137
jintao <hellojintao@protonmail.com>
2025-05-28 20:05:27 -0500

    chore: update batch contract

--- apps/batch-transactions/src/UrsaToken.sol
+++ apps/batch-transactions/src/UrsaToken.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "solmate/tokens/ERC20.sol";
- 
+import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
+
 contract UrsaToken is ERC20 {
+    address public owner;
+
     constructor() ERC20("Ursa Token", "URSA", 18) {
-        _mint(msg.sender, 1000000 * 10**18); // Mint 1 million tokens
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
     }
 } 
