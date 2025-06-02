d410f7b164873300e39bcaee14c1179befc4a4f1
jintao <hellojintao@protonmail.com>
2025-06-02 08:55:14 -0500

    feat: implement batch transaction execution with tests

--- apps/batch-transactions/src/BatchTransaction.sol
+++ apps/batch-transactions/src/BatchTransaction.sol
@@ -14,19 +14,27 @@ contract BatchTransaction {
     }
 
     // Events
-    event BatchExecuted(address indexed executor, uint256 transactionCount);
+    event BatchExecuted(address indexed executor, uint256 nonce, uint256 transactionCount);
     event TransactionFailed(uint256 index, bytes reason);
 
     // Constants
     uint256 public constant MAX_BATCH_SIZE = 100;
 
+    // Mapping to track used nonces for replay protection
+    mapping(address => uint256) public nonces;
+
     /**
      * @dev Execute a batch of transactions
      * @param transactions Array of transactions to execute
+     * @param nonce The nonce for this batch execution
      */
-    function execute(Transaction[] calldata transactions) external {
+    function execute(Transaction[] calldata transactions, uint256 nonce) external {
         require(transactions.length <= MAX_BATCH_SIZE, "Batch too large");
         require(transactions.length > 0, "Empty batch");
+        require(nonce == nonces[msg.sender], "Invalid nonce");
+
+        // Increment nonce before execution to prevent reentrancy
+        nonces[msg.sender]++;
 
         for (uint256 i = 0; i < transactions.length; i++) {
             Transaction calldata transaction = transactions[i];
@@ -38,6 +46,15 @@ contract BatchTransaction {
             }
         }
 
-        emit BatchExecuted(msg.sender, transactions.length);
+        emit BatchExecuted(msg.sender, nonce, transactions.length);
+    }
+
+    /**
+     * @dev Get the current nonce for an address
+     * @param account Address to get nonce for
+     * @return Current nonce
+     */
+    function getNonce(address account) external view returns (uint256) {
+        return nonces[account];
     }
 } 
