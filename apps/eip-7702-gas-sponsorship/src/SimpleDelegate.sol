64af72d1f82b3a49f94e63d42bb836ae50efb629
IchiBera <ichiraku@berachain.com>
2025-06-04 01:21:47 -0500

    Run forge fmt on eip-7702 guide contracts

--- apps/eip-7702-gas-sponsorship/src/SimpleDelegate.sol
+++ apps/eip-7702-gas-sponsorship/src/SimpleDelegate.sol
@@ -9,12 +9,11 @@ import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/Messa
 /// @dev Since the EOA prepares their own transactions (via a UI, their own code, etc.), they will have full reign on what is in their transaction. Thus having generic implementation contract code that an EOA points to for carrying out low level calls is acceptable.
 /// @dev This contract, as is, showcases gas sponsorship. Later parts of the EIP-7702 gas sponsorship guides will show: 1.) Initializing a contract, that will become a specific contract to an EOA basically, 2.) Having a check to ensure that the signer is the EOA for a sponsored call using solidity scripts and foundry to construct the signed transaction. Code commented out pertains to these later parts.
 contract SimpleDelegate {
-    
     /// State Vars
     struct Call {
-    bytes data;
-    address to;
-    uint256 value;
+        bytes data;
+        address to;
+        uint256 value;
     }
 
     /// Error Statements
@@ -26,30 +25,29 @@ contract SimpleDelegate {
     event Reimbursed(address indexed sponsor, uint256 refund);
 
     function execute(Call memory userCall, address sponsor, uint256 nonce) external payable {
-    // Begin gas tracking
-    uint256 startGas = gasleft();
+        // Begin gas tracking
+        uint256 startGas = gasleft();
 
-    // Execute the user-intended call
-    (bool success, ) = userCall.to.call{value: userCall.value}(userCall.data);
-    if (!success) revert ExternalCallFailed();
+        // Execute the user-intended call
+        (bool success,) = userCall.to.call{value: userCall.value}(userCall.data);
+        if (!success) revert ExternalCallFailed();
 
-    // Calculate gas used and refund
-    uint256 gasUsed = startGas - gasleft();
-    uint256 gasCost = gasUsed * tx.gasprice;
-    uint256 refund = (msg.value > gasCost) ? (msg.value - gasCost) : 0;
+        // Calculate gas used and refund
+        uint256 gasUsed = startGas - gasleft();
+        uint256 gasCost = gasUsed * tx.gasprice;
+        uint256 refund = (msg.value > gasCost) ? (msg.value - gasCost) : 0;
 
-    if (refund > 0) {
-        (bool refunded, ) = sponsor.call{value: refund}("");
-        require(refunded, ReimbursementFailed());
-        emit Reimbursed(sponsor, refund);
-    }
+        if (refund > 0) {
+            (bool refunded,) = sponsor.call{value: refund}("");
+            require(refunded, ReimbursementFailed());
+            emit Reimbursed(sponsor, refund);
+        }
 
-    emit HenloSaid(msg.sender, address(this), "Henlo triggered!");
+        emit HenloSaid(msg.sender, address(this), "Henlo triggered!");
     }
 
     function getNonceToUse(uint256 currentEOANonce) external pure returns (uint256) {
-    uint256 nonceToUse = currentEOANonce + 10;
-    return nonceToUse;
+        uint256 nonceToUse = currentEOANonce + 10;
+        return nonceToUse;
     }
-
 }
