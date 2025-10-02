15d0ac7f28c77f838f2209eaec0527c231a8bb96
jintao <jintao@berachain.com>
2025-10-02 18:37:13 -0500

    chore: update for base <> berachain bridge

--- apps/layerzero-oft/src/MyOFT.sol
+++ apps/layerzero-oft/src/MyOFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {OFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+import { OFT } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyOFT is OFT {
     constructor(
